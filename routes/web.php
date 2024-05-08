238377a0a43a4a931af660fbe676c793d1ace8b1
mileprogramer <obecano.ispunjeno@gmail.com>
2024-05-08 23:32:20 +0200

    Adding routes for category add, edit, destroy, return

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\AddPost;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\PostController;
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
 
@@ -38,20 +42,25 @@ Route::get('/admin/login', function () {
 Route::get('/admin', function () {
     return view("adminDashboard/admin");
 });
+
 // POSTS
-Route::get('/admin/posts', function () {
-    return view("adminDashboard/all_posts");
-});
-Route::get('/admin/add-post', function () {
-    return view("adminDashboard/add_post");
-});
+Route::get('/admin/posts', [PostController::class, 'index']);
+Route::get('/admin/add-post', [AddPost::class, 'create']);
+
 // Categories
-Route::get('/admin/categories', function () {
-    return view("adminDashboard/all_categories");
+Route::get('/admin/categories', [CategoryController::class, 'index']);
+
+Route::view('/admin/add-category', "adminDashboard.category.add_category");
+Route::get("/admin/category/delete/{name_category}", [CategoryController::class, "destroy"]);
+Route::get("/admin/category/return/{name_category}", [CategoryController::class, "retunCategory"]);
+
+Route::get("/admin/category/edit/{name_category}", function($category) {
+    return view("adminDashboard.category.edit_category", ["name_category"=>$category]);
 });
-Route::get('/admin/add-category', function () {
-    return view("adminDashboard/add_category");
-});
+
+Route::post('/admin/add-category', [CategoryController::class, 'store']);
+Route::post('/admin/edit-category/', [CategoryController::class, 'edit']);
+
 // TAGS
 Route::get('/admin/tags', function () {
     return view("adminDashboard/all_tags");
