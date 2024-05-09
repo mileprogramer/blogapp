a1216d9a28fbf2c7f0bf4489123f71db3bb80937
mileprogramer <obecano.ispunjeno@gmail.com>
2024-05-09 18:42:59 +0200

    Strtolower case function

--- resources/views/adminDashboard/category/category.blade.php
+++ resources/views/adminDashboard/category/category.blade.php
@@ -1,7 +1,7 @@
 <div class="card col-3 p-0">
     <div class="card-header"><h3>{{ $category["name_category"] }}</h3></div>
     <div class="card-footer">
-        <a class="btn btn-danger" href="/admin/category/delete/{{ strtolower($category["name_category"]) }}">Delete</a>
-        <a class="btn btn-warning" href="/admin/category/edit/{{ strtolower($category["name_category"]) }}">Edit</a>
+        <a class="btn btn-danger" href="/admin/category/delete/{{ $category["name_category"] }}">Delete</a>
+        <a class="btn btn-warning" href="/admin/category/edit/{{ $category["name_category"] }}">Edit</a>
     </div>
 </div>
