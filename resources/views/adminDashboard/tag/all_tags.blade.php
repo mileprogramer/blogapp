a23f32ca517c3948c5531acb95c82f7890f6f3f1
mileprogramer <obecano.ispunjeno@gmail.com>
2024-05-11 16:12:45 +0200

    Adding alert on successfully  deleted tag

--- resources/views/adminDashboard/tag/all_tags.blade.php
+++ resources/views/adminDashboard/tag/all_tags.blade.php
@@ -17,34 +17,44 @@
 @endsection
 
 @section('main-wrapper')
-  <!-- Main wrapper - style you can find in pages.scss -->
-  <div id="main-wrapper" data-layout="vertical" data-navbarbg="skin5" data-sidebartype="full" data-sidebar-position="absolute" data-header-position="absolute" data-boxed-layout="full">
-      <!-- Topbar header - style you can find in pages.scss -->
-      @include('adminDashboard.partials.header')
-      <!-- Left Sidebar - style you can find in sidebar.scss  -->
-      @include('adminDashboard.partials.aside')
-      <!-- Page wrapper  -->
-      <div class="page-wrapper">
-          <div class="container">
-              <h1 class="p-3">All tags</h1>
-              <div class="row gap-3">
-                @foreach ($all_tags as $tag)
-                    <div class="card col-4 p-0">
-                        <div class="card-header">Tag name: <h4 class="d-inline">{{ $tag["tag_name"] }}</h4> </div>
-                        <div class="card-footer">
-                            <a href="/admin/delete-tag/{{ $tag["slug"] }}" class="btn btn-danger">Delete</a>
-                            <a href="/admin/edit-tag/{{ $tag["slug"] }}" class="btn btn-warning">Edit</a>
-                        </div>
+    <!-- Main wrapper - style you can find in pages.scss -->
+    <div id="main-wrapper" data-layout="vertical" data-navbarbg="skin5" data-sidebartype="full" data-sidebar-position="absolute"
+        data-header-position="absolute" data-boxed-layout="full">
+        <!-- Topbar header - style you can find in pages.scss -->
+        @include('adminDashboard.partials.header')
+        <!-- Left Sidebar - style you can find in sidebar.scss  -->
+        @include('adminDashboard.partials.aside')
+        <!-- Page wrapper  -->
+        <div class="page-wrapper">
+            <div class="container">
+                <h1 class="p-3">All tags</h1>
+                @if (session('success') !== null)
+                    <div class="alert alert-success">
+                        <p class="d-inline">{{ session('success') }}</p>
+                        <img class="d-inline float-end btn-close-alert" src="{{ asset('img/close-alert.svg') }}"
+                            alt="" style="width: 30px; margin-top: -6px;">
                     </div>
-                @endforeach
-              </div>
-          </div>
-      </div>
-      <!-- End Page wrapper  -->
-  </div>
+                @endif
+                <div class="row gap-3">
+                    @foreach ($all_tags as $tag)
+                        <div class="card col-4 p-0">
+                            <div class="card-header">Tag name: <h4 class="d-inline">{{ $tag['tag_name'] }}</h4>
+                            </div>
+                            <div class="card-footer">
+                                <a href="/admin/delete-tag/{{ $tag['slug'] }}" class="btn btn-danger">Delete</a>
+                                <a href="/admin/edit-tag/{{ $tag['slug'] }}" class="btn btn-warning">Edit</a>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+        <!-- End Page wrapper  -->
+    </div>
 @endsection
 
 @section('js')
-  <!-- End Wrapper -->
-  @include('adminDashboard.partials.js')
+    <!-- End Wrapper -->
+    <script src="{{ asset('admin_dashboard/dist/js/alert.js') }}"></script>
+    @include('adminDashboard.partials.js')
 @endsection
