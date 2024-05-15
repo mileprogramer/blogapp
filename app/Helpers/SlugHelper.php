55a4048c1068fc4d5ff5e481d741331121cfb30f
mileprogramer <obecano.ispunjeno@gmail.com>
2024-05-15 22:39:24 +0200

    Refactoring code

--- app/Helpers/SlugHelper.php
+++ app/Helpers/SlugHelper.php
@@ -4,9 +4,9 @@ namespace App\Helpers;
 
 class SlugHelper
 {
-    public static function generateSlug($tag_name)
+    public static function generateSlug($potenial_slug)
     {
-        $hasSpace = explode(" ", $tag_name);
+        $hasSpace = explode(" ", $potenial_slug);
         $slug = isset($hasSpace[1]) ? implode("-", $hasSpace) : $hasSpace[0];
         return strtolower($slug);
     }
