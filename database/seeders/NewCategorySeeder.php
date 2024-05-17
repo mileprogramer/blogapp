3933ad5f8012a4844675e6ab1212e05d5a3152c9
mileprogramer <obecano.ispunjeno@gmail.com>
2024-05-17 19:19:59 +0200

    Refactoring category seeder

--- database/seeders/NewCategorySeeder.php
+++ database/seeders/NewCategorySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -13,8 +14,21 @@ class NewCategorySeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table("category")->insert([
-            "name_category"=> "Sports"
-        ]);
+        $category1 = new Category();
+        $category1->name_category = "Sports";
+        $category1->save();
+
+        $category2 = new Category();
+        $category2->name_category = "Politics";
+        $category2->save();
+        
+        $category3 = new Category();
+        $category3->name_category = "Programming";
+        $category3->save();
+        
+        $category4 = new Category();
+        $category4->name_category = "Fashion";
+        $category4->save();
+
     }
 }
