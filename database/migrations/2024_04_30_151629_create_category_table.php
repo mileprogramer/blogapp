9b890b0d9df92a961cac7db5a2045671991d4661
mileprogramer <obecano.ispunjeno@gmail.com>
2024-05-18 18:27:03 +0200

    Add slug column in category table

--- database/migrations/2024_04_30_151629_create_category_table.php
+++ database/migrations/2024_04_30_151629_create_category_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('category', function (Blueprint $table) {
             $table->id();
             $table->string('name_category');
+            $table->string('slug');
             $table->boolean('active')->default(true);
             $table->timestamps();
         });
